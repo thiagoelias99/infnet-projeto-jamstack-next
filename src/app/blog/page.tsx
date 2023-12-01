7efd0f75ade8ab5ab41646181b0ff10e9cb1f9c8
Thiago Elias <thiagoelias99@gmail.com>
2023-12-01 10:51:44 -0300

    feat: added home & blog pages

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const BlogPage = () => {
+    return (
+        <main className='max-w-[1024px] m-auto mt-36 md:mt-24'>
+            <h1 className='text-3xl md:text-4xl lg:text-5xl text-center my-4'>Blog</h1>
+            <h2 className='text-xl md:text-2xl lg:text-3xl p-4'>Veja nossos posts</h2>
+            <nav className='flex flex-col justify-start items-start gap-4'>
+            </nav>
+        </main>
+    )
+}
+
+export default BlogPage
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+import Image from 'next/image'
+
+import HomeCover from '../images/home-cover.png'
+
 export default function Home() {
   return (
     <main>
-      <h1>Home Page</h1>
+      <figure className='flex max-h-[900px] relative mt-32 md:mt-24'>
+        <div className='w-full absolute top-[40%] text-center z-20'>
+          <p className='text-2xl md:text-5xl text-white uppercase'>inspirações para viajar</p>
+        </div>
+        <div className='absolute w-full h-full bg-black/25 z-10'></div>
+        <Image
+          src={HomeCover}
+          alt='mountains'
+          className='max-h-[900px] z-0 bg-cover w-full'
+        />
+      </figure>
+
+      <section className='p-4 max-w-[1024px] m-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-10 gap-y-10'>
+        <h2 className='col-span-1 md:col-span-2 lg:col-span-3 text-3xl md:text-4xl lg:text-5xl text-center mt-4'>Últimos Posts</h2>
+      </section>
     </main>
   )
 }
