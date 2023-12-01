57b589c919ed8778bffd3d5b3690f291e759eba1
Thiago Elias <thiagoelias99@gmail.com>
2023-12-01 11:13:03 -0300

    fix: added y scrollbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
-      <body className='w-screen min-h-screen m-auto flex flex-col justify-between overflow-hidden'>
+      <body className='w-screen min-h-screen m-auto flex flex-col justify-between overflow-x-hidden'>
         <Header />
         {children}
         <Footer />
