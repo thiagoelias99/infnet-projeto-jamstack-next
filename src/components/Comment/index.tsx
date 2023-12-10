9db6b5ef9275bcae7e324cdcf9ae498879a41fd2
Thiago Elias <thiagoelias99@gmail.com>
2023-12-10 11:55:30 -0300

    feat: added delete button

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -1,12 +1,19 @@
+import { XOctagon } from 'lucide-react'
 import { IComments } from '@/models/Comments'
 
 interface CommentProps {
   comment: IComments
+  showDelete?: boolean
 }
 
-const PostComment = ({ comment }: CommentProps) => {
+const PostComment = ({ comment, showDelete = false }: CommentProps) => {
   return (
-    <div className="flex flex-col items-start border w-full p-2 mb-2 rounded">
+    <div className="flex flex-col items-start border w-full p-2 mb-2 rounded relative">
+      {showDelete && (
+        <div className='absolute -right-2 -top-1' role='button'>
+          <XOctagon className="h-5 w-5 stroke-red-500 bg-transparent" />
+        </div>
+      )}
       <div className='flex justify-between items-center w-full'>
         <p className='font-bold text-sm'>{comment.userName}</p>
         <p className='italic text-sm'>em {new Date(comment.createdAt).toLocaleString()}</p>
